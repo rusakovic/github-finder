7526f9933bf6febe3196150ea91aaa49256f238e
rusakovic <maxrusakovich@gmail.com>
2019-12-04 15:38:54 +0300

    Updates

---
+++ precache-manifest.eb803e444e731afb95373d400f7ead65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0caab8f49e3fa1090cdcafc705695b07",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "2e028696c4d946e25471",
+    "url": "/github-finder/static/css/main.bf14e704.chunk.css"
+  },
+  {
+    "revision": "ac4d3e18c0a840a07cf8",
+    "url": "/github-finder/static/js/2.c60fb237.chunk.js"
+  },
+  {
+    "revision": "2e028696c4d946e25471",
+    "url": "/github-finder/static/js/main.f4e0a863.chunk.js"
+  },
+  {
+    "revision": "66cd455262ebb3dfa747",
+    "url": "/github-finder/static/js/runtime-main.b8459382.js"
+  }
+]);
