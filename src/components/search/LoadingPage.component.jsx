3e9ff31b9e3189334d0ba2cb14170aac8631e8f9
rusakovic <maxrusakovich@gmail.com>
2019-12-04 15:57:17 +0300

    updating link to loading spinner

--- src/components/search/LoadingPage.component.jsx
+++ src/components/search/LoadingPage.component.jsx
@@ -6,7 +6,7 @@ const LoadingPage = () => {
      <img 
       className="loader__image" 
       alt="loading-spinner" 
-      src="/images/loader.gif" 
+      src="images/loader.gif" 
     />
     </div>
   )
